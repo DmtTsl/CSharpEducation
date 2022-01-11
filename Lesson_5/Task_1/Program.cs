e1ddf7dc2a5a1f8f8a10fda146e539d15dc8ac37
Dmitry Tselikhin <d-ts@yandex.ru>
2022-01-11 18:41:04 +0300

    Lesson 5 added

---
+++ Lesson_5/Task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(Split(GetSentence()));
+            Console.ReadKey();
+            
+        }
+        static string GetSentence()
+        {
+            Console.WriteLine("Type a sentence");
+            string sentence = Console.ReadLine();
+            return sentence;
+        }
+        static string[] Split(string s)
+        {
+            string[] mass = s.Split(' ');
+            return mass;
+        }
+        static void Print (string [] mass)
+        {
+            foreach(string s in mass)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Lesson_5/Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseWords(GetSentence());
+            Console.ReadKey();
+
+        }
+        static string GetSentence()
+        {
+            Console.WriteLine("Type a sentence");
+            string sentence = Console.ReadLine();
+            return sentence;
+        }
+        static string[] Split(string s)
+        {
+            string[] mass = s.Split(' ');
+            return mass;
+        }
+        static void ReverseWords(string inputPhrase)
+
+        {
+            string[] mass = Split(inputPhrase);
+            Array.Reverse(mass);
+            string resultString = default;
+            foreach(string s in mass)
+            {
+                resultString += s + " ";
+            }
+            Console.WriteLine(resultString);
+            
+        }
+        
+    }
+}
