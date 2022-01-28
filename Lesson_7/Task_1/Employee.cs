1920bee379a21c18333292a71a6b1b940d866e70
Dmitry Tselikhin <d-ts@yandex.ru>
2022-01-28 20:56:22 +0300

    Lesson_7 added

---
+++ Lesson_7/Task_1/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    struct Employee
+    {
+        
+        public int ID { get; set; }
+        public DateTime Now { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+
+        public Employee(string[] args)
+        {
+            this.ID = int.Parse(args[0]);
+            this.Now = Convert.ToDateTime(args[1]);
+            this.Name = args[2];
+            this.Age = int.Parse(args[3]);
+            this.Height = int.Parse(args[4]);
+            this.DateOfBirth = Convert.ToDateTime(args[5]);
+            this.PlaceOfBirth = args[6];
+        }
+        
+        public string DataToWrite()
+        {
+            string s = $"{ID}#{Now}#{Name}#{Age}#{Height}#{DateOfBirth.ToShortDateString()}#{PlaceOfBirth}";
+            return s;
+        }
+
+        public string DataToShow()
+        {
+            string s = $"{ID}\t{Now}\t{Name}\t{Age,10}\t{Height}\t{DateOfBirth.ToShortDateString()}\t{PlaceOfBirth}";
+            return s;
+
+        }
+        
+
+    }
+}
---
+++ Lesson_7/Task_1/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace Lesson_7
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+        start:
+            Console.WriteLine("Выберите что делать\n1 - выбрать существующий файл\n2 - создать новый файл");
+            string fileName = Actions.New1OrExisting2File();
+
+        proceed:
+            string[] lines = Actions.ReadFile(fileName);
+            ListOfEmp employees = new ListOfEmp(lines);
+        
+            if (employees.Count() == 0)
+            {
+                Console.WriteLine("Выберите что делать\n1 - добавить новую запись\n2 - выбрать другой файл\n3 - закрыть программу");
+                int choice = Actions.ChoiceOf3();
+                switch (choice)
+                {
+                    case 1:
+                        employees.Add(fileName);
+                        goto proceed;
+
+                    case 2: goto start;
+                    case 3: return;
+                }
+            }
+            else if (employees.Count() == 1)
+            {
+
+                Console.WriteLine("Выберите что делать\n1 - прочитать запись\n2 - добавить новую запись\n3 - изменить запись\n" +
+                    "4 - удалить запись\n5 - выбрать другой файл\n6 - закрыть программу");
+                int choice = Actions.ChoiceOf6();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите ID сотрудника для вывода записи или 0, чтобы вывести все записи");
+                        employees.Show(Actions.TryParse());
+                        goto proceed;
+
+                    case 2:
+                        employees.Add(fileName);
+                        goto proceed;
+
+                    case 3:
+                        employees.Edit(fileName);
+                        goto proceed;
+
+                    case 4:
+                        employees.Erase(fileName);
+                        goto proceed;
+
+                    case 5: goto start;
+
+                    case 6: return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Выберите что делать\n1 - прочитать запись\n2 - добавить новую запись\n3 - изменить запись\n" +
+                    "4 - удалить запись\n5 - вывод записей по диапазону дат\n6 - вывод всех записей, отсортированных по дате\n" +
+                    "7 - выбрать другой файл\n8 - закрыть программу");
+                int choice = Actions.ChoiceOf8();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите ID сотрудника для вывода записи или 0, чтобы вывести все записи");
+                        employees.Show(Actions.TryParse());
+                        goto proceed;
+
+                    case 2:
+                        employees.Add(fileName);
+                        goto proceed;
+
+                    case 3:
+                        employees.Edit(fileName);
+                        goto proceed;
+
+                    case 4:
+                        employees.Erase(fileName);
+                        goto proceed;
+
+                    case 5:
+                        employees.ShowDates();
+                        goto proceed;
+
+                    case 6:
+                        employees.DateSorting();
+                        goto proceed;
+
+                    case 7: goto start;
+
+                    case 8: return;
+                }
+
+
+
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+       
+            
+
+            
+        
+        static void Greet()
+        {
+            Console.WriteLine("Выберите что делать\n1 - вывести информацию из файла на экран\n2 - добавить новую запись в файл");
+          
+        }
+        
+        
+        
+
+        
+        /// <summary>
+        /// собирает данные для записи в файл
+        /// </summary>
+        /// <returns>кортеж значений данных</returns>
+        
+       
+        static void WriteFile((int ID, string Name, int Age, int Height, string DateOfBirth, string PlaceOfBirth) tuple, string fileName)
+        {
+            
+            string dateTime = DateTime.Now.ToString();
+            string dataToWrite = $"{tuple.ID}#{dateTime}#{tuple.Name}#{tuple.Age}#{tuple.Height}#{tuple.DateOfBirth}#{tuple.PlaceOfBirth}";
+            StreamWriter streamWriter = new StreamWriter(fileName,true);
+            streamWriter.WriteLine(dataToWrite);
+            streamWriter.Close();
+        }
+    }
+}
