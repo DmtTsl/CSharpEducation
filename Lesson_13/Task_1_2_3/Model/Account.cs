446e5c53e20eb8cfc522137836fadbe99df70af5
Dmitry Tselikhin <d-ts@yandex.ru>
2022-06-25 14:54:48 +0300

    Lesson_13

---
+++ Lesson_13/Task_1_2_3/Model/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2_3
+{
+    abstract class Account
+    {
+        string AccountNumber { get; set; }
+        double Sum { get; set; }
+        public Account()
+        {
+            if (AccountNumberRepository.freeNumber.Count > 0)
+            {
+                AccountNumberRepository.freeNumber.Sort();
+                AccountNumber = AccountNumberRepository.freeNumber.First();
+                AccountNumberRepository.freeNumber.Remove(AccountNumber);
+                
+            }
+            else
+            {
+                AccountNumber = AccountNumberRepository.accountNumberCount + 1;
+                
+            }
+            
+        }
+    }
+}
---
+++ Lesson_13/Task_1_2_3/Repository/AccountNumberRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task_1_2_3
+{
+    public static class AccountNumberRepository
+    {              
+        public static string accountNumberCount = File.ReadAllText("AccNumCount.txt");
+        public static List<string> freeNumber = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText("FreeNumber.json"));
+        public static void SaveData()
+        {
+            File.WriteAllText("AccNumCount.txt", accountNumberCount);
+            File.WriteAllText("FreeNumber.json", JsonConvert.SerializeObject(freeNumber));
+        }
+        
+    }
+}
