e4024309b1bca173276c745672168ef478a94ae1
Dmitry Tselikhin <d-ts@yandex.ru>
2022-07-11 21:42:58 +0300

    Some spaces removed

--- Lesson_13/Task_1_2_3/Commands/RelayCommand.cs
+++ Lesson_13/Task_1_2_3/Commands/RelayCommand.cs
@@ -7,24 +7,20 @@ namespace Task_1_2_3
     {
         private Action<object> _execute;
         private Func<object, bool> _canExecute;
-
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
-
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             this._execute = execute;
             this._canExecute = canExecute;
         }
-
         public bool CanExecute(object parameter)
         {
             return this._canExecute == null || this._canExecute(parameter);
         }
-
         public void Execute(object parameter)
         {
             this._execute(parameter);
